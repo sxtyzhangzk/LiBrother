6067808b88bf9b1cf07b74dd12730de6034e3dc9
xuyao1997 <xuyao1997@sjtu.edu.cn>
2015-11-19 17:03:06 +0800

    book library极其session对应部分的权限和异常处理

--- LiBrother/LiBrotherServer/session.h
+++ LiBrother/LiBrotherServer/session.h
@@ -2,7 +2,7 @@
 #define __SESSION_H__
 
 #include <string>
-
+#include "common_types.h"
 struct ILibClassFactory;
 class CSession
 {
@@ -11,10 +11,15 @@ public:
 	bool startSession(ILibClassFactory * pClassFactory, const std::string& strClientIP);
 	bool stopSession();
 	void recvRequest(const std::string& strRequest, std::string& strResponse);
+	int getCurrentAuthLevel();
+	int getCurrentReadLevel();
 
 protected:
 	ILibClassFactory * m_pClassFactory;
 	std::string m_strClientIP;
+	int user_id;
+	int current_read_level;
+	int current_auth_level;
 };
 
 #endif
